47f22fc4a524a476da9a6298b637e37322d52067
penjarla sarala <penjarlasarala15@gmail.com>
2024-10-31 10:07:53 +0530

    change template

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "mlProject"
+project_name = "house_price"
 
 list_of_files = [
     f"src/{project_name}/__init__.py",
